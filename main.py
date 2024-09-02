114330f913cef5f181746aeea9f7170d8ac7294c
Marcel Bonkowski <marcel.bonkowski@web.de>
2024-09-02 10:24:02 +0200

    add report

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
 	text = get_book_text(book_path)
 	num_words = get_num_words(text)
 	chars_dict = get_chars_dict(text)
-	print(chars_dict)
+	format_report(book_path, num_words, chars_dict)
 
 def get_num_words(text) -> int:
 	count = text.split()
@@ -24,6 +24,28 @@ def get_chars_dict(text) -> {}:
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def sort_on(dict):
+    return dict["num"]
+
+def format_report(path, num_words, chars_dict):
+	char_list = []
+	for char in chars_dict:
+		if char.isalpha():
+			char_list.append({"name": char, "num": chars_dict[char]})
+
+	char_list.sort(reverse=True, key=sort_on)
+
+	report = f"""
+--- Begin report of {path} ---
+{num_words} words found in the document
+	"""
+
+	for char in char_list:
+		report = report + f"\n The '{char["name"]}' character was found {char["num"]} times"
+
+	report = report + "\n --- End report ---"
+	print(report)
 
 if __name__ == "__main__":
     main()
